f21e72825f6d808823453b3b9780a52df5cba94f
phananhduc2003 <pananhduc2003@gmail.com>
2023-08-09 15:31:05 +0700

    fix header s1

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -52,6 +52,13 @@ function Search() {
         setShowResult(false);
     };
 
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
     return (
         <HeadlessTippy
             interactive
@@ -74,7 +81,7 @@ function Search() {
                     value={searchValue}
                     placeholder="Search accounts and video"
                     spellCheck={false}
-                    onChange={(e) => setSearchValue(e.target.value)}
+                    onChange={handleChange}
                     onFocus={() => setShowResult(true)}
                 />
                 {!!searchValue && !loading && (
@@ -85,7 +92,7 @@ function Search() {
 
                 {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
-                <button className={cx('search-btn')}>
+                <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
                     <SearchIcon />
                 </button>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
+import routesConfig from '~/config/routes';
+
 //Lauouts
 import { HeaderOnly } from '~/components/Layout';
 
+// Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
@@ -8,10 +11,10 @@ import Upload from '~/pages/Upload';
 
 //public routers
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/:nickname', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: routesConfig.home, component: Home },
+    { path: routesConfig.following, component: Following },
+    { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
 ];
 //private routers
 const privateRoutes = [];
