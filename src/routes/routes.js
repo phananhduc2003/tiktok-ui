1fd1a42db0e0e2bb579110da24eb37630282a545
phananhduc2003 <pananhduc2003@gmail.com>
2023-08-13 14:48:48 +0700

    xu ly nhieu file index && toi uu

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import config from '~/config';
+
+//Lauouts
+import { HeaderOnly } from '~/layouts';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+//public routers
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+];
+//private routers
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
